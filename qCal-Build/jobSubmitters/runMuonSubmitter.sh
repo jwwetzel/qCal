e6fda4276b4d589ec96938519ea18a2172fbdb59
James Wetzel <jwwetzel@gmail.com>
2019-04-17 17:56:48 -0500

    edited submitters

--- qCal-Build/jobSubmitters/runMuonSubmitter.sh
+++ qCal-Build/jobSubmitters/runMuonSubmitter.sh
@@ -5,5 +5,9 @@
 
 for E in 1 2 3 4 5 6 7 8 9 10 15 20 25 30 40 50 60 70 80 90 100 150 200 250 300 350 400 450 500 750 1000
 do
+	if [ "$E" -eq 2 ] || [ "$E" -eq 400 ] || [ "$E" -eq 60 ]
+	then
+		continue
+	fi
 	./muonSubmitter.sh muons $E /Users/jwwetzel/geant4Simulations/qCal/qCal-Build $1
 done
