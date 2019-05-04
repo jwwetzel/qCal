d2843768d196785724d195fe57382eb3b2951d11
James Wetzel <jwwetzel@gmail.com>
2019-05-04 18:27:34 -0500

    changed job submission

--- qCal-Build/jobSubmitters/runMuonSubmitter.sh
+++ qCal-Build/jobSubmitters/runMuonSubmitter.sh
@@ -3,6 +3,7 @@
 ###
 ### $1 = number of events
 
+
 for E in 1 2 3 4 5 6 7 8 9 10 15 20 25 30 40 50 60 70 80 90 100 150 200 250 300 350 400 450 500 750 1000
 do
 #	if [ "$E" -eq 30 ] || [ "$E" -eq 40 ] || [ "$E" -eq 70 ]  || [ "$E" -eq 90 ]  || [ "$E" -eq 200 ] || [ "$E" -eq 400 ]  || [ "$E" -eq 15 ]  || [ "$E" -eq 1000 ]
@@ -10,5 +11,12 @@ do
 #		./muonSubmitter.sh muons $E /Users/jwwetzel/geant4Simulations/qCal/qCal-Build $1
 #		continue
 #	fi
-	./muonSubmitter.sh muons $E /Users/jwwetzel/geant4Simulations/qCal/qCal-Build $1
+
+	if [ ! -d /nfsscratch/Users/jwwetzel/$E ]
+	then
+    		mkdir /nfsscratch/Users/jwwetzel/$E
+        	wait
+	fi
+
+	qsub -V -j y -q INFORMATICS muonSubmitter.job muons $E /Users/jwwetzel/geant4Simulations/qCal/qCal-Build $1
 done
